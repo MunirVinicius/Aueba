238eb83eeb148cdf5c7fbbac592230589ede2ecd
MunirVinicius <munir.vinicius11@gmail.com>
2021-09-03 21:45:56 -0300

    DB leak fix

--- Controllers/CharController.cs
+++ Controllers/CharController.cs
@@ -53,7 +53,7 @@ namespace aueba.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CharID,CharName,CharDex,CharHP")] Char @char)
+        public async Task<IActionResult> Create([Bind("CharID,CharName,CharDex,CharHP")] Models.Char @char)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace aueba.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CharID,CharName,CharDex,CharHP")] Char @char)
+        public async Task<IActionResult> Edit(int id, [Bind("CharID,CharName,CharDex,CharHP")] Models.Char @char)
         {
             if (id != @char.CharID)
             {
